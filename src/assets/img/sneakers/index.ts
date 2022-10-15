c508f5f09537e285db214486d2f7478231b023c2
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-10-15 23:25:17 +0300

    fix: исправление и доработка вёрстки, грид-контейнера;

---
+++ src/assets/img/sneakers/index.ts
@@ -0,0 +1,15 @@
+import NB373 from './373.png'
+import NB574 from './574.png'
+import NB670 from './670.png'
+import NB920 from './920.png'
+import NB5740 from './5740.png'
+import NBXR from './x-racer.png'
+
+export {
+   NB373,
+   NB574,
+   NB670,
+   NB920,
+   NB5740,
+   NBXR,
+}
